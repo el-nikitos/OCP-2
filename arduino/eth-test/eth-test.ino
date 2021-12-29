1809a8e69898d659790903f11ea3d5f362a0a2b7
nikitos <el-nikitos@mail.ru>
2021-12-29 23:12:49 +0300

    определена кастомные функции работы с gpio

--- arduino/eth-test/eth-test.ino
+++ arduino/eth-test/eth-test.ino
@@ -5,7 +5,7 @@
 #define DOUT_2    7
 #define DOUT_3    6
 #define DOUT_4    5
-//#define DOUT_5 
+#define DOUT_5    0xB7
 #define ETH_RES   9
 
 static uint8_t mac[6] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};
@@ -15,19 +15,24 @@ static uint16_t port = 80;
 boolean DOUT_1_turnon = false,
         DOUT_2_turnon = false,
         DOUT_3_turnon = false,
-        DOUT_4_turnon = false;
+        DOUT_4_turnon = false,
+        DOUT_5_turnon = false,
+        DOUT_6_turnon = false,
+        DOUT_7_turnon = false,
+        DOUT_8_turnon = false,
+        DOUT_9_turnon = false,
+        DOUT_10_turnon = false;
 
 ETHER_28J60 ethernet;
-
-char* str_ref_turn_on = " Turn on</A>",
-      str_ref_turn_off = " Turn off</A>";
 
 void setup() {
   pinMode(DOUT_1, OUTPUT);
   pinMode(DOUT_2, OUTPUT);
   pinMode(DOUT_3, OUTPUT);
   pinMode(DOUT_4, OUTPUT);
-  //pinMode(DOUT_5, OUTPUT);
+  
+  _pinMode(DOUT_5, OUTPUT);
+  
   pinMode(ETH_RES, OUTPUT);
 
   digitalWrite(ETH_RES, HIGH);
@@ -35,9 +40,11 @@ void setup() {
   digitalWrite(DOUT_2, LOW);
   digitalWrite(DOUT_3, LOW);
   digitalWrite(DOUT_4, LOW);
-  //digitalWrite(DOUT_5, LOW);
+  _digitalWrite(DOUT_5, LOW);
 
   ethernet.setup(mac, ip, port);
+
+  delay(10000);
 }
 
 void loop() {
@@ -58,36 +65,67 @@ void loop() {
 }
 
 void http_TX(){
-  ethernet.print("<H1>DOUT channales</H1>");
-  int i = 1;
+  ethernet.print("<A HREF='/'>refresh</A></p>\n");
 
   if (DOUT_1_turnon == true)  {
     http_dout_draw( "H", "1", "off" );
   } else {
     http_dout_draw( "L", "1", "on" );
   }
-  ethernet.print("</p>");
   //
   if (DOUT_2_turnon == true)  {
     http_dout_draw( "H", "2", "off" );
   } else {
     http_dout_draw( "L", "2", "on" );
   }
-  ethernet.print("</p>");
   //
   if (DOUT_3_turnon == true)  {
     http_dout_draw( "H", "3", "off" );
   } else {
     http_dout_draw( "L", "3", "on" );
   }
-  ethernet.print("</p>");
   //
   if (DOUT_4_turnon == true)  {
     http_dout_draw( "H", "4", "off" );
   } else {
     http_dout_draw( "L", "4", "on" );
   }
-  ethernet.print("</p>");
+  //
+  if (DOUT_5_turnon == true)  {
+    http_dout_draw( "H", "5", "off" );
+  } else {
+    http_dout_draw( "L", "5", "on" );
+  }
+  //
+  if (DOUT_6_turnon == true)  {
+    http_dout_draw( "H", "6", "off" );
+  } else {
+    http_dout_draw( "L", "6", "on" );
+  }
+  //
+  if (DOUT_7_turnon == true)  {
+    http_dout_draw( "H", "7", "off" );
+  } else {
+    http_dout_draw( "L", "7", "on" );
+  }
+  //
+  if (DOUT_8_turnon == true)  {
+    http_dout_draw( "H", "8", "off" );
+  } else {
+    http_dout_draw( "L", "8", "on" );
+  }
+  //
+  if (DOUT_9_turnon == true)  {
+    http_dout_draw( "H", "9", "off" );
+  } else {
+    http_dout_draw( "L", "9", "on" );
+  }
+  //
+  if (DOUT_10_turnon == true)  {
+    http_dout_draw( "H", "10", "off" );
+  } else {
+    http_dout_draw( "L", "10", "on" );
+  }
   //
 }
 
@@ -97,45 +135,52 @@ void http_dout_draw(char* str_status, char* str_channel, char* str_new_status) {
   ethernet.print( ":" );
   ethernet.print( str_status );
   ethernet.print( "</b>" );
-  ethernet.print( "<A HREF='out" );
+  ethernet.print( "<A HREF='" );
   ethernet.print( str_channel );
   ethernet.print( str_new_status );
   ethernet.print( "'> Turn " );
   ethernet.print( str_new_status );
   ethernet.print( "</A>" );
 
+  ethernet.print("</p>\n");
 }
 
 void request_execution(char* http_get){
-  if (strcmp(http_get, "out1on") == 0) {
+  if (strcmp(http_get, "1on") == 0) {
     DOUT_1_turnon = true;
   }
-  if (strcmp(http_get, "out1off") == 0) {
+  if (strcmp(http_get, "1off") == 0) {
     DOUT_1_turnon = false;
   }
   //
-  if (strcmp(http_get, "out2on") == 0) {
+  if (strcmp(http_get, "2on") == 0) {
     DOUT_2_turnon = true;
   }
-  if (strcmp(http_get, "out2off") == 0) {
+  if (strcmp(http_get, "2off") == 0) {
     DOUT_2_turnon = false;
   }
   //
   
-  if (strcmp(http_get, "out3on") == 0) {
+  if (strcmp(http_get, "3on") == 0) {
     DOUT_3_turnon = true;
   }
-  if (strcmp(http_get, "out3off") == 0) {
+  if (strcmp(http_get, "3off") == 0) {
     DOUT_3_turnon = false;
   }
   //
-  if (strcmp(http_get, "out4on") == 0) {
+  if (strcmp(http_get, "4on") == 0) {
     DOUT_4_turnon = true;
   }
-  if (strcmp(http_get, "out4off") == 0) {
+  if (strcmp(http_get, "4off") == 0) {
     DOUT_4_turnon = false;
   }
-  
+  //
+  if (strcmp(http_get, "5on") == 0) {
+    DOUT_5_turnon = true;
+  }
+  if (strcmp(http_get, "5off") == 0) {
+    DOUT_5_turnon = false;
+  }
   //
 }
 
@@ -162,5 +207,11 @@ void update_outputs() {
     digitalWrite(DOUT_4, HIGH);
   } else {
     digitalWrite(DOUT_4, LOW);
+  }
+
+  if (DOUT_5_turnon == true)  {
+    _digitalWrite(DOUT_5, HIGH);
+  } else {
+    _digitalWrite(DOUT_5, LOW);
   }
 }
