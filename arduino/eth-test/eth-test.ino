e210dc5cc9acf3dacbbcd058c25250aba26f50d2
nikitos <el-nikitos@mail.ru>
2022-01-01 14:35:19 +0300

    настроены DI и DO для мастера + web-лицо

--- arduino/eth-test/eth-test.ino
+++ arduino/eth-test/eth-test.ino
@@ -15,16 +15,25 @@
 */
 #define ETH_RES   0xB1
 
+#define DIN_1     A1
+#define DIN_2     A0
+#define DIN_3     A7
+#define DIN_4     A6
+#define DIN_5     A2
+#define DIN_6     A3
+#define DIN_7     3
+#define DIN_8     4
+
 static uint8_t mac[6] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};
 static uint8_t ip[4] = {192, 168, 137, 15};
 static uint16_t port = 80;
 
 ETHER_28J60 ethernet;
 
-byte byte_m_output = 0, // 0b[RES, RES, DOUT_6, DOUT_5,  DOUT_4, DOUT_3, DOUT_2, DOUT_1]
-     byte_s_output = 0, // 0b[RES, RES,    RES,    RES, DOUT_10, DOUT_9, DOUT_8, DOUT_7]
-     byte_m_input = 0,
-     byte_s_input = 0;
+byte byte_m_output = 0, // 0b[RES,   RES,   DOUT_6, DOUT_5, DOUT_4,  DOUT_3, DOUT_2, DOUT_1]
+     byte_s_output = 0, // 0b[RES,   RES,   RES,    RES,    DOUT_10, DOUT_9, DOUT_8, DOUT_7]
+     byte_m_input = 0,  // 0b[DIN_8, DIN_7, DIN_6,  DIN_5,  DIN_4,   DIN_3,  DIN_2,  DIN_1]
+     byte_s_input = 0;  // 0b[RES,   RES,   RES,    RES,    DIN_12,  DIN_11, DIN_10, DIN_9]
 
 void setup() {
   _pinMode(DOUT_1, OUTPUT);
@@ -63,144 +72,46 @@ void loop() {
   
   //test_blink();
   update_outputs();
+  update_inputs();
   delay(10);
 }
 
 void http_TX(){
-  if ( (byte_m_output&0b00000001)>0 )  {
-    http_gpio_draw( "H", "1", "off", true );
-  } else {
-    http_gpio_draw( "L", "1", "on", true );
-  }
   //
-  if ( (byte_m_output&0b00000010)>0 )  {
-    http_gpio_draw( "H", "2", "off", true );
-  } else {
-    http_gpio_draw( "L", "2", "on", true );
+  http_open_table_draw( "DOUT" );
+  http_grid_name_draw( 10 );
+  http_dout_status_draw();
+  /*
+  ethernet.print( "\n" );
+  for (int i=0;i<150;i++) {
+    ethernet.print( " " );
   }
+  ethernet.print( "\n" );
+  */
+  http_dout_commands_draw();
+
+  //ethernet.print( "</table>\n" );
+  http_close_table_draw();
   //
-  if ( (byte_m_output&0b00000100)>0 )  {
-    http_gpio_draw( "H", "3", "off", true );
-  } else {
-    http_gpio_draw( "L", "3", "on", true );
-  }
-  //
-  if ( (byte_m_output&0b00001000)>0 )  {
-    http_gpio_draw( "H", "4", "off", true );
-  } else {
-    http_gpio_draw( "L", "4", "on", true );
-  }
-  //
-  if ( (byte_m_output&0b00010000)>0 )  {
-    http_gpio_draw( "H", "5", "off", true );
-  } else {
-    http_gpio_draw( "L", "5", "on", true );
-  }
-  //
-  if ( (byte_m_output&0b00100000)>0 )  {
-    http_gpio_draw( "H", "6", "off", true );
-  } else {
-    http_gpio_draw( "L", "6", "on", true );
-  }
-  //
-  if ( (byte_s_output&0b00000001)>0 )  {
-    http_gpio_draw( "H", "7", "off", true );
-  } else {
-    http_gpio_draw( "L", "7", "on", true );
-  }
-  //
-  if ( (byte_s_output&0b00000010)>0 )  {
-    http_gpio_draw( "H", "8", "off", true );
-  } else {
-    http_gpio_draw( "L", "8", "on", true );
-  }
-  //
-  if ( (byte_s_output&0b00000100)>0 )  {
-    http_gpio_draw( "H", "9", "off", true );
-  } else {
-    http_gpio_draw( "L", "9", "on", true );
-  }
-  //
-  if ( (byte_s_output&0b00001000)>0 )  {
-    http_gpio_draw( "H", "10", "off", true );
-  } else {
-    http_gpio_draw( "L", "10", "on", true );
-  }
+  http_open_table_draw( "DIN" );
+  http_grid_name_draw( 12 );
+  http_din_status_draw();
+  //ethernet.print( "</table>\n" );
+  http_close_table_draw();
   //
   ethernet.print("<A HREF='/'>refresh</A></p>\n");
   //
-  if ( (byte_m_input&0b00000001)>0 )  {
-    http_gpio_draw( "H", "1", "", false );
-  } else {
-    http_gpio_draw( "L", "1", "", false );
-  }
-  //
-  if ( (byte_m_input&0b00000010)>0 )  {
-    http_gpio_draw( "H", "2", "", false );
-  } else {
-    http_gpio_draw( "L", "2", "", false );
-  }
-  //
-  if ( (byte_m_input&0b00000100)>0 )  {
-    http_gpio_draw( "H", "3", "", false );
-  } else {
-    http_gpio_draw( "L", "3", "", false );
-  }
-  //
-  if ( (byte_m_input&0b00001000)>0 )  {
-    http_gpio_draw( "H", "4", "", false );
-  } else {
-    http_gpio_draw( "L", "4", "", false );
-  }
-  //
-  if ( (byte_m_input&0b00010000)>0 )  {
-    http_gpio_draw( "H", "5", "", false );
-  } else {
-    http_gpio_draw( "L", "5", "", false );
-  }
-  //
-  if ( (byte_m_input&0b00100000)>0 )  {
-    http_gpio_draw( "H", "6", "", false );
-  } else {
-    http_gpio_draw( "L", "6", "", false );
-  }
-  //
-  if ( (byte_m_input&0b01000000)>0 )  {
-    http_gpio_draw( "H", "7", "", false );
-  } else {
-    http_gpio_draw( "L", "7", "", false );
-  }
-  //
-  if ( (byte_m_input&0b10000000)>0 )  {
-    http_gpio_draw( "H", "8", "", false );
-  } else {
-    http_gpio_draw( "L", "8", "", false );
-  }
-  //
-  if ( (byte_s_input&0b00000001)>0 )  {
-    http_gpio_draw( "H", "9", "", false );
-  } else {
-    http_gpio_draw( "L", "9", "", false );
-  }
-  //
-  if ( (byte_s_input&0b00000010)>0 )  {
-    http_gpio_draw( "H", "10", "", false );
-  } else {
-    http_gpio_draw( "L", "10", "", false );
-  }
-  //
-  if ( (byte_s_input&0b00000100)>0 )  {
-    http_gpio_draw( "H", "11", "", false );
-  } else {
-    http_gpio_draw( "L", "11", "", false );
-  }
-  //
-  if ( (byte_s_input&0b00001000)>0 )  {
-    http_gpio_draw( "H", "12", "", false );
-  } else {
-    http_gpio_draw( "L", "12", "", false );
-  }
-  //
+}
+
+void http_open_table_draw(char* str_caption) {
+  ethernet.print( "<table border='1'>" );
+  ethernet.print( "<caption>" );
+  ethernet.print( str_caption );
+  ethernet.print( "</caption>\n" );
+}
+
+void http_close_table_draw()  {
+  ethernet.print( "</table>\n" );
 }
 
 void http_gpio_draw(char* str_status, char* str_channel, char* str_new_status, boolean b_dout) {
@@ -226,75 +137,189 @@ void http_gpio_draw(char* str_status, char* str_channel, char* str_new_status, b
   ethernet.print("</p>\n");
 }
 
+void http_grid_name_draw(byte byte_count) {
+  ethernet.print( "<tr>" );
+  //
+  for (int i=1;i<=byte_count;i++)  {
+    ethernet.print( "<th>" );
+    ethernet.print( i );
+    ethernet.print( "</th>" );
+  }
+  //
+  ethernet.print( "</tr>\n" );
+}
+
+void http_dout_status_draw() {
+  ethernet.print( "<tr>" );
+  //
+  draw_grid_data( byte_m_output&0b00000001 );
+  //
+  draw_grid_data( byte_m_output&0b00000010 );
+  //
+  draw_grid_data( byte_m_output&0b00000100 );
+  //
+  draw_grid_data( byte_m_output&0b00001000 );
+  //
+  draw_grid_data( byte_m_output&0b00010000 );
+  //
+  draw_grid_data( byte_m_output&0b00100000 );
+  //
+  draw_grid_data( byte_s_output&0b00000001 );
+  //
+  draw_grid_data( byte_s_output&0b00000010 );
+  //
+  draw_grid_data( byte_s_output&0b00000100 );
+  //
+  draw_grid_data( byte_s_output&0b00001000 );
+  //
+  ethernet.print( "</tr>\n" );
+}
+
+void http_din_status_draw() { // функция не переопределена для дискретных входов!
+  ethernet.print( "<tr>" );
+  //
+  draw_grid_data( byte_m_input&0b00000001 );
+  //
+  draw_grid_data( byte_m_input&0b00000010 );
+  //
+  draw_grid_data( byte_m_input&0b00000100 );
+  //
+  draw_grid_data( byte_m_input&0b00001000 );
+  //
+  draw_grid_data( byte_m_input&0b00010000 );
+  //
+  draw_grid_data( byte_m_input&0b00100000 );
+  //
+  draw_grid_data( byte_m_input&0b01000000 );
+  //
+  draw_grid_data( byte_m_input&0b10000000 );  // входа на мастере
+  //
+  draw_grid_data( byte_s_input&0b00000001 );
+  //
+  draw_grid_data( byte_s_input&0b00000010 );
+  //
+  draw_grid_data( byte_s_input&0b00000100 );
+  //
+  draw_grid_data( byte_s_input&0b00001000 );
+  //
+  ethernet.print( "</tr>\n" );
+}
+
+void http_dout_commands_draw() {
+  ethernet.print( "<tr>" );
+  //
+  draw_grid_command( byte_m_output&0b00000001, "1" );
+  //
+  draw_grid_command( byte_m_output&0b00000010, "2" );
+  //
+  draw_grid_command( byte_m_output&0b00000100, "3" );
+  //
+  draw_grid_command( byte_m_output&0b00001000, "4" );
+  //
+  draw_grid_command( byte_m_output&0b00010000, "5" );
+  //
+  draw_grid_command( byte_m_output&0b00100000, "6" );
+  //
+  draw_grid_command( byte_s_output&0b00000001, "7" );
+  //
+  draw_grid_command( byte_s_output&0b00000010, "8" );
+  //
+  draw_grid_command( byte_s_output&0b00000100, "9" );
+  //
+  draw_grid_command( byte_s_output&0b00001000, "10" );
+  //
+  ethernet.print( "</tr>\n" );
+}
+
+void draw_grid_data(byte byte_status) {
+  if ( byte_status>0 )  {
+    ethernet.print( "<td>H</td>" );
+  } else {
+    ethernet.print( "<td>L</td>" );
+  }
+}
+
+void draw_grid_command(byte byte_status, char* str_channel) {
+  if ( byte_status>0 )  {
+    ethernet.print( "<td><A HREF='" );
+    ethernet.print( str_channel );
+    ethernet.print( "L'>OFF</A></td>" );
+  } else {
+    ethernet.print( "<td><A HREF='" );
+    ethernet.print( str_channel );
+    ethernet.print( "H'>ON</A></td>" );
+  }
+}
+
 void request_execution(char* http_get){
-  if (strcmp(http_get, "1on") == 0) {
+  if (strcmp(http_get, "1H") == 0) {
     byte_m_output |= (1<<0);
   }
-  if (strcmp(http_get, "1off") == 0) {
+  if (strcmp(http_get, "1L") == 0) {
     byte_m_output &= ~(1<<0);
   }
   //
-  if (strcmp(http_get, "2on") == 0) {
+  if (strcmp(http_get, "2H") == 0) {
     byte_m_output |= (1<<1);
   }
-  if (strcmp(http_get, "2off") == 0) {
+  if (strcmp(http_get, "2L") == 0) {
     byte_m_output &= ~(1<<1);
   }
   //
   
-  if (strcmp(http_get, "3on") == 0) {
+  if (strcmp(http_get, "3H") == 0) {
     byte_m_output |= (1<<2);
   }
-  if (strcmp(http_get, "3off") == 0) {
+  if (strcmp(http_get, "3L") == 0) {
     byte_m_output &= ~(1<<2);
   }
   //
-  if (strcmp(http_get, "4on") == 0) {
+  if (strcmp(http_get, "4H") == 0) {
     byte_m_output |= (1<<3);
   }
-  if (strcmp(http_get, "4off") == 0) {
+  if (strcmp(http_get, "4L") == 0) {
     byte_m_output &= ~(1<<3);
   }
   //
-  if (strcmp(http_get, "5on") == 0) {
+  if (strcmp(http_get, "5H") == 0) {
     byte_m_output |= (1<<4);
   }
-  if (strcmp(http_get, "5off") == 0) {
+  if (strcmp(http_get, "5L") == 0) {
     byte_m_output &= ~(1<<4);
   }
   //
-  if (strcmp(http_get, "6on") == 0) {
+  if (strcmp(http_get, "6H") == 0) {
     byte_m_output |= (1<<5);
   }
-  if (strcmp(http_get, "6off") == 0) {
+  if (strcmp(http_get, "6L") == 0) {
     byte_m_output &= ~(1<<5);
   }
   //
-  if (strcmp(http_get, "7on") == 0) {
+  if (strcmp(http_get, "7H") == 0) {
     byte_s_output |= (1<<0);
   }
-  if (strcmp(http_get, "7off") == 0) {
+  if (strcmp(http_get, "7L") == 0) {
     byte_s_output &= ~(1<<0);
   }
   //
-  if (strcmp(http_get, "8on") == 0) {
+  if (strcmp(http_get, "8H") == 0) {
     byte_s_output |= (1<<1);
   }
-  if (strcmp(http_get, "8off") == 0) {
+  if (strcmp(http_get, "8L") == 0) {
     byte_s_output &= ~(1<<1);
   }
   //
-  if (strcmp(http_get, "9on") == 0) {
+  if (strcmp(http_get, "9H") == 0) {
     byte_s_output |= (1<<2);
   }
-  if (strcmp(http_get, "9off") == 0) {
+  if (strcmp(http_get, "9L") == 0) {
     byte_s_output &= ~(1<<2);
   }
   //
-  if (strcmp(http_get, "10on") == 0) {
+  if (strcmp(http_get, "10H") == 0) {
     byte_s_output |= (1<<3);
   }
-  if (strcmp(http_get, "10off") == 0) {
+  if (strcmp(http_get, "10L") == 0) {
     byte_s_output &= ~(1<<3);
   }
   //
@@ -361,4 +386,64 @@ void update_outputs() {
     _digitalWrite(DOUT_10, LOW);
   }
 */
+}
+
+void update_inputs() {
+  int a_in = 0;
+
+  a_in = analogRead(DIN_1);
+  if (a_in>500) {
+    byte_m_input |= (1<<0);
+  } else {
+    byte_m_input &= ~(1<<0);
+  }
+
+  a_in = analogRead(DIN_2);
+  if (a_in>500) {
+    byte_m_input |= (1<<1);
+  } else {
+    byte_m_input &= ~(1<<1);
+  }
+
+  a_in = analogRead(DIN_3);
+  if (a_in>500) {
+    byte_m_input |= (1<<2);
+  } else {
+    byte_m_input &= ~(1<<2);
+  }
+
+  a_in = analogRead(DIN_4);
+  if (a_in>500) {
+    byte_m_input |= (1<<3);
+  } else {
+    byte_m_input &= ~(1<<3);
+  }
+
+  a_in = analogRead(DIN_5);
+  if (a_in>500) {
+    byte_m_input |= (1<<4);
+  } else {
+    byte_m_input &= ~(1<<4);
+  }
+
+  a_in = analogRead(DIN_6);
+  if (a_in>500) {
+    byte_m_input |= (1<<5);
+  } else {
+    byte_m_input &= ~(1<<5);
+  }
+
+  //a_in = analogRead(DIN_7);
+  if ((digitalRead(DIN_7))==HIGH) {
+    byte_m_input |= (1<<6);
+  } else {
+    byte_m_input &= ~(1<<6);
+  }
+
+  //a_in = analogRead(DIN_8);
+  if ((digitalRead(DIN_8))==HIGH) {
+    byte_m_input |= (1<<7);
+  } else {
+    byte_m_input &= ~(1<<7);
+  }
 }
